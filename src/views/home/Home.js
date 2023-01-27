0f4a722025ab20b2710a1cf27f9cbdd88a86abc5
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-27 13:44:36 +0100

    structure and responsive menu

---
+++ src/views/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Menu from "../../components/menu/Menu";
+import { Container } from "@mui/system";
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+        <Menu />
+      </Container>
+    </>
+  );
+};
+
+export default Home;
