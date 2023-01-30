293cb2bf94330ff3f4470d051b5c44ac74ee4cee
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-30 15:42:09 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './views/home/Home'
+import Home from './views/Home'
+import About from './components/about/About'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
       </Routes>
     </Router>
   );
