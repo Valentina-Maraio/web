8fcad53776a4e719eaad3f54fa1c31c0ae376f30
Valentina-Maraio <valentina.maraio89@gmail.com>
2023-01-31 17:16:47 +0100

    coools

--- src/components/scroll/Scroll.js
+++ src/components/scroll/Scroll.js
@@ -1,19 +1,21 @@
 import React from "react";
-import './style/Scroll.css'
+import "./style/Scroll.css";
+import { Grid, Divider } from "@mui/material";
 
 const Scroll = () => {
   return (
     <>
       <div className="scroll">
         <h5>SCROLL</h5>
-        |
-        <br />
-        |
-        <br />
-        |
-        <br />
-        |
-        <br />
+        <Grid container>
+          <Grid item xs>
+            <h3 className="invisible_text">Valentina</h3>
+          </Grid>
+          <Divider variant="middle" orientation="vertical" flexItem>
+          </Divider>
+          <Grid item xs>
+          </Grid>
+        </Grid>
       </div>
     </>
   );
